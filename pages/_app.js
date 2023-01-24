1f0d9d41f0b1c9fbc469c7b67436c3cc2d302487
runj33t <ranjeetmaurya576@gmail.com>
2023-01-24 14:33:09 +0530

    fixed styled component sync problem with nextjs by adding bablerc and _document file

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import "../public/css/styles.css"
-import Navbar from "../components/Navbar";
 
 const MyApp = ({Component, pageProps}) => {
     return (
         <div>
-            <Navbar />
             <Component {...pageProps} />
         </div>
     );
